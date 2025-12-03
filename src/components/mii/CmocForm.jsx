a4a9e64af697a5a3e1178fa54568a7a787374056
t0g3pii <info@t0g3pii.de>
2025-12-03 19:33:02 +0000

    Disabled CMOC due to disabled services.

--- src/components/mii/CmocForm.jsx
+++ src/components/mii/CmocForm.jsx
@@ -1,33 +1,48 @@
-import { Form } from 'react-bootstrap';
+import { Alert, Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
-function CmocForm({ value, handleChange, handleBlur, error, touched }) {
+function CmocForm({
+  value,
+  handleChange,
+  handleBlur,
+  error,
+  touched,
+  disabled,
+}) {
   return (
-    <Form.Group className="text-center" controlId="cmoc-entry-no">
-      <Form.Label>Check Mii Out Channel Entry Number</Form.Label>
-      <Form.Control
-        type="text"
-        placeholder="Check Mii Out Channel Entry Number"
-        name="cmocEntryNo"
-        onChange={handleChange}
-        onBlur={handleBlur}
-        isInvalid={!!error}
-        isValid={touched && !error}
-        value={value}
-      />
-      <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
-      <Form.Text className="text-muted">
-        You can browse all Miis from the Check Mii Out Channel on{' '}
-        <a
-          href="https://mii.rc24.xyz/"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          mii.rc24.xyz
-        </a>
-        . The entry number can be entered with or without dashes
-      </Form.Text>
-    </Form.Group>
+    <>
+      <Alert variant="warning" className="text-start">
+        The Check Mii Out Channel integration is currently disabled while the
+        WiiLink team is reworking CMOC. Editing CMOC Miis is temporarily not
+        available.
+      </Alert>
+      <Form.Group className="text-center" controlId="cmoc-entry-no">
+        <Form.Label>Check Mii Out Channel Entry Number</Form.Label>
+        <Form.Control
+          type="text"
+          placeholder="Check Mii Out Channel Entry Number"
+          name="cmocEntryNo"
+          onChange={handleChange}
+          onBlur={handleBlur}
+          isInvalid={!!error}
+          isValid={touched && !error}
+          value={value}
+          disabled={disabled}
+        />
+        <Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
+        <Form.Text className="text-muted">
+          You can browse all Miis from the Check Mii Out Channel on{' '}
+          <a
+            href="https://mii.rc24.xyz/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            mii.rc24.xyz
+          </a>
+          . The entry number can be entered with or without dashes
+        </Form.Text>
+      </Form.Group>
+    </>
   );
 }
 
@@ -37,10 +52,12 @@ CmocForm.propTypes = {
   handleBlur: PropTypes.func.isRequired,
   error: PropTypes.string,
   touched: PropTypes.object.isRequired,
+  disabled: PropTypes.bool,
 };
 
 CmocForm.defaultProps = {
   error: null,
+  disabled: false,
 };
 
 export default CmocForm;
